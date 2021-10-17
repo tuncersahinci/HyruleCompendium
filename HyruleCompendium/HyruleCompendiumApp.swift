2ffa40d8e875a2a3b5aac7f0ae23390e9f2e5ff0
Denerod <40545015+Denerod@users.noreply.github.com>
2021-10-17 15:26:15 +0300

    Initial Commit

---
+++ HyruleCompendium/HyruleCompendiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyruleCompendiumApp.swift
+//  HyruleCompendium
+//
+//  Created by Tuncer Şahinci on 17.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HyruleCompendiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
